8d5794581b51384cfbdda0ac4bf83df8d8e4d800
xingjiepan <xingjiepan@gmail.com>
2018-01-30 11:27:31 -0800

    Implement the score_structure.py file

---
+++ simple_operations/score_structure.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+'''Score a structure.
+Usage:
+    ./score_structure.py pdb_file.pdb
+'''
+
+import sys
+
+import pyrosetta
+from pyrosetta import rosetta
+
+
+if __name__ == '__main__':
+    pyrosetta.init()
+
+    # Load pose
+
+    pdb_file = sys.argv[1]
+    pose = rosetta.core.pose.Pose()
+    rosetta.core.import_pose.pose_from_file(pose, pdb_file)
+
+    # Score
+
+    sfxn = rosetta.core.scoring.get_score_function()
+    sfxn(pose)
+
+    pose.dump_pdb('scored_pose.pdb')
