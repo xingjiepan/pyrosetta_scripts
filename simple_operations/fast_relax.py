dca6efc152c1e224c2a6458ec2388f95421641a4
xingjiepan <xingjiepan@gmail.com>
2018-11-06 13:47:29 -0800

    Update the fast relax script

--- simple_operations/fast_relax.py
+++ simple_operations/fast_relax.py
@@ -11,13 +11,25 @@ import pyrosetta
 from pyrosetta import rosetta
 
 
-def fast_relax(pose):
+def fast_relax(pose, fast_relax_rounds=1, use_ex_rotamers=True):
     '''Idealize a pose.'''
     rosetta.basic.options.set_boolean_option('relax:constrain_relax_to_start_coords', True)
-    rosetta.basic.options.set_boolean_option('relax:ramp_constraints', False)
+    rosetta.basic.options.set_boolean_option('relax:ramp_constraints', True)
+
+    task_factory = rosetta.core.pack.task.TaskFactory()
+    
+    if use_ex_rotamers:
+        ers = rosetta.core.pack.task.operation.ExtraRotamersGeneric()
+        ers.ex1(True)
+        ers.ex2(True)
+        ers.extrachi_cutoff(18)
+        task_factory.push_back(ers)
+    
+    task_factory.push_back(rosetta.core.pack.task.operation.RestrictToRepacking())
+    lac = rosetta.protocols.task_operations.LimitAromaChi2Operation()
+    task_factory.push_back(lac)
 
     sfxn = rosetta.core.scoring.get_score_function()
-    fast_relax_rounds = 1
     fast_relax_mover = rosetta.protocols.relax.FastRelax(sfxn, fast_relax_rounds)
 
     fast_relax_mover.apply(pose)
